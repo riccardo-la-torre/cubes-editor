64d670cdc60a4ca3b7b3b5c6bebb7b0e860e1c40
Riccardo <riccardo@MML1002.fritz.box>
2020-10-15 20:45:04 +0100

    Publish demo to github pages

---
+++ docs/precache-manifest.b9391440f0d80101f0d6e7143a053864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617b5010ccb067b2ef3ecf5044e0b2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb37deec14db62d33de",
+    "url": "/static/css/main.5fd108e9.chunk.css"
+  },
+  {
+    "revision": "bd1ac4b6f87cd0a4869d",
+    "url": "/static/js/2.beb9f305.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.beb9f305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb37deec14db62d33de",
+    "url": "/static/js/main.0dc2ac1a.chunk.js"
+  },
+  {
+    "revision": "d32b4856ebfd389dbfa7",
+    "url": "/static/js/runtime-main.abf8bf39.js"
+  }
+]);
